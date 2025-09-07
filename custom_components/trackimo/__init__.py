6abcf85266334160071b94014780ed9c6df717d8
awms <andy@andyshepherd.tv>
2025-09-07 19:39:22 -0400

    fix: UI configuration and NameError

--- custom_components/trackimo/__init__.py
+++ custom_components/trackimo/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigEntry
+from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.typing import ConfigType
